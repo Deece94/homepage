e219bf1642e1e95d99c1dab323bad5d636d9f597
Deece94 <Dcollar@hotmail.com.au>
2022-08-19 19:57:40 +1000

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,6 @@ import styled from "styled-components";
 import NavBar from "./components/NavBar";
 import Theme from "./components/Theme";
 
-const Text = styled.p`
-  background-color: ${(props) => props.theme.colors.dark};
-  color: ${(props) => props.theme.colors.white};
-`;
-
 export default function App() {
   return (
     <Theme>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import Theme from "./Theme";
 
 const NavContainer = styled.nav`
   background-color: ${(props) => props.theme.colors.dark};
