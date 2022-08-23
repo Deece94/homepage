6aff4ea46a63c14b39a28cc777625ac9da9b1a95
Deece94 <dcollar@hotmail.com.au>
2022-08-23 11:04:15 +1000

    started about component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { createGlobalStyle } from "styled-components";
+import About from "./components/About";
 import NavBar from "./components/NavBar";
 import Theme from "./components/Theme";
 
 export default function App() {
-  return (
-    <>
-      <Theme>
-        <div className="App">
-          <NavBar />
-        </div>
-      </Theme>
-    </>
-  );
+	return (
+		<>
+			<Theme>
+				<div className="App">
+					<NavBar />
+					<About />
+				</div>
+			</Theme>
+		</>
+	);
 }
---
+++ src/components/About.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Header = styled.h3``;
+
+export default function About() {
+	return (
+		<div id="about">
+			<Header>About Me</Header>
+			<p>information</p>
+		</div>
+	);
+}
