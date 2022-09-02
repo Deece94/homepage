dbc8e2d0d79318d568af9f76598c51d6cee7fdc7
Deece94 <dcollar@hotmail.com.au>
2022-09-02 15:37:46 +1000

    added contact component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import GlobalWrapper from "./components/GlobalWrapper";
 import Home from "./components/Home";
@@ -7,17 +8,18 @@ import Theme from "./components/Theme";
 import Work from "./components/Work";
 
 export default function App() {
-  return (
-    <>
-      <Theme>
-        <NavBar />
-        <GlobalWrapper className="App">
-          <Home />
-          <About />
-          <Work />
-          <Footer />
-        </GlobalWrapper>
-      </Theme>
-    </>
-  );
+	return (
+		<>
+			<Theme>
+				<NavBar />
+				<GlobalWrapper className="App">
+					<Home />
+					<About />
+					<Work />
+					<Contact />
+					<Footer />
+				</GlobalWrapper>
+			</Theme>
+		</>
+	);
 }
