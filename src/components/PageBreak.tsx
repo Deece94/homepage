b3897c7caabeef393b5edf711ea3149835bcbafb
Deece94 <dcollar@hotmail.com.au>
2022-09-14 15:44:21 +1000

    modified page break line

--- src/components/PageBreak.tsx
+++ src/components/PageBreak.tsx
@@ -1,17 +1,17 @@
 import styled from "styled-components";
 
 const BreakDiv = styled.div`
-	height: 20px;
+    height: 20px;
 `;
 
 const Line = styled.hr`
-	border: 0.5px solid ${(props) => props.theme.colors.primary};
+    border: 0.5px solid ${(props) => props.theme.colors.dark_accent};
 `;
 
 export default function PageBreak() {
-	return (
-		<BreakDiv>
-			<Line />
-		</BreakDiv>
-	);
+    return (
+        <BreakDiv>
+            <Line />
+        </BreakDiv>
+    );
 }
