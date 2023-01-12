993bc85e2b8ba2e0efc979fe1e3fc29f929570cd
Deece94 <dcollar@hotmail.com.au>
2023-01-12 16:26:14 +1100

    Changed about me text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -49,22 +49,34 @@ export default function About() {
             <Container>
                 <Half>
                     <Paragraph>
-                        Hello! My name is Dylan. I'm a fullstack web developer
-                        from Newcastle, Australia. I first found an interest in
-                        programming during high school building games with
-                        GameMaker and Flash and pulling apart others to see how
-                        they ticked. Now I love building products that enhance
-                        your experience on the web.
+                        Hello! My name is Dylan. I'm a full-stack web developer
+                        from Newcastle, Australia. My passion for programming
+                        was ignited during my high school days, where I spent my
+                        time building games with tools such as GameMaker and
+                        Flash, and deconstructing existing ones to understand to
+                        see how they ticked. Today, I am driven by the challenge
+                        of building exceptional digital products that enrich the
+                        online experience for users.
                     </Paragraph>
                     <Paragraph>
-                        Currently I work for{" "}
+                        Currently I work at{" "}
                         <Link href="https://www.4dcontrols.com">
                             4D Controls
                         </Link>
                         , one of Australia's largest suppliers of automation
-                        equipment. I focus on designing, building and
-                        maintaining applications that improve the way we run on
-                        a day to day basis.
+                        equipment. I specialize in crafting innovative,
+                        efficient and user-friendly web applications that
+                        streamline daily operations and enhance productivity. My
+                        work encompasses the entire development process from
+                        conceptualization, design and implementation, to ongoing
+                        maintenance and optimization.
+                    </Paragraph>
+                    <Paragraph>
+                        My skills in a variety of programming languages,
+                        including HTML, CSS, JavaScript, and PHP, allow me to
+                        create highly functional, visually stunning websites and
+                        applications. If you want to hear more, feel free to get
+                        in contact!
                     </Paragraph>
                 </Half>
                 <Half>
